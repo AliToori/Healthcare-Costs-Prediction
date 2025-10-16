ec26095fc98036f2821f6255f8faf6439ecd3e61
AliToori <alituri512@gmail.com>
2025-10-17 02:19:28 +0500

    Healthcare Costs Prediction Model using LR

--- HealthcareCostsPredictor.py
+++ HealthcareCostsPredictor.py
@@ -1,14 +1,5 @@
 import pandas as pd
-import numpy as np
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import layers
-from sklearn.preprocessing import LabelEncoder, StandardScaler
-from sklearn.model_selection import train_test_split
-import matplotlib.pyplot as plt
-import urllib.request
-import os
-
+import numpy
 
 class HealthCostPredictor:
     """A class to predict healthcare costs using TensorFlow Linear Regression."""
